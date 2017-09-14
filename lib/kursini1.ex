771a42c1f9e98c7a6f749860a19f4eec90dd7c5f
Ezols <Edvards.ozols@gmail.com>
2017-09-14 21:50:11 +0300

    uzd2, padot sarakstu un integeri ar ko dalit

--- lib/kursini1.ex
+++ lib/kursini1.ex
@@ -3,5 +3,8 @@ defmodule Kursini1 do
   def even(integers) when is_list(integers) do
     Enum.filter(integers, fn(integer) -> rem(integer, 2) == 0 end)
   end
-  
+
+  def divide(integers, divider) when is_list(integers) do
+    Enum.filter(integers, fn(integer) -> rem(integer, divider) == 0 end)
+  end
 end
